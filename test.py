e90ae454a54f32856dc83f12a11ef26d7689223a
hikaly <said047@qq.com>
2017-08-03 11:48:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -50,39 +50,4 @@ def file_upload(remote_ip, login_user, login_pw, file_path, save_path):
         print flag_str
 
 if __name__ == '__main__':
-    # 模拟服
-    # pw_backup('106.75.93.166', 'ubuntu', '52Xiyou@jianke', 'db', '/home/ubuntu/login_info.txt')
-
-    # 游戏工场-支付
-    # pw_backup('114.215.134.133', 'root', 'Funship@ytjh', 'db', '/root/pay_server/netError/2017-06-29-16-19-23.html')
-
-    # 游戏工场-登录
-    # pw_backup('114.215.134.133', 'root', 'Funship@ytjh', 'db', '/root/NewLoginServer/netError/', True)
-
-    # 游戏工场-游戏1
-    # pw_backup('106.75.21.31', 'ubuntu', 'www.52xiyou.com', 'db', '/root/trunk/netError/', True)
-
-    # 游戏工场-游戏2
-    # pw_backup('121.42.24.116', 'root', 'Funship@ytjh', 'db/yxgc', '/root/trunk/netError/', True)
-
-    # 腾讯云 登录服
-    # pw_backup('118.89.61.50', 'ubuntu', 'www.52xiyou.com', 'db', '/home/ubuntu/login_server/netError/', True)
-
-    # 游戏测试服
-    # pw_backup('106.75.18.220', 'ubuntu', '52Xiyou@jianke', 'db/ceshi', '/data/trunk/netError/', True)
-
-    # 测试登录服
-    # pw_backup('106.75.18.220', 'ubuntu', '52Xiyou@jianke', 'db',  '/data/NewLoginServer/netError/', True)
-
-    # 正式支付服
-    # pw_backup('106.75.61.163', 'ubuntu', '52Xiyou@jianke', 'db', '/data/pay_server/netError/', True)
-
-    # 测试支付服
-    # pw_backup('106.75.61.163', 'ubuntu', '52Xiyou@jianke', 'db', '/data/project/pay_server/netError/', True)
-
-    # 三国测试
-    # pw_backup('106.75.25.205', 'ubuntu', '52Xiyou@jianke', 'db/sanguo', '/data/trunk/netError/', True)
-
-    # 正式服
-    pw_backup('106.75.61.165', 'ubuntu', '52Xiyou@jianke', 'db/linan', '/data/trunk/netError/', True)
     pass
